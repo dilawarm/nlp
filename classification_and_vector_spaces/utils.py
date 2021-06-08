a56525b351096bb5955679a8a26796bdfdab7edf
Dilawar Mahmood <dilawarmm@outlook.com>
2021-06-08 09:19:02 +0200

    stuff

---
+++ classification_and_vector_spaces/utils.py
@@ -0,0 +1,149 @@
+import re
+import string
+import numpy as np
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import TweetTokenizer
+
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+
+
+def process_tweet(tweet):
+    """
+    Input:
+        tweet: a string containing a tweet
+    Output:
+        tweets_clean: a list of words containing the processed tweet
+
+    """
+    stemmer = PorterStemmer()
+    stopwords_english = stopwords.words("english")
+    # remove stock market tickers like $GE
+    tweet = re.sub(r"\$\w*", "", tweet)
+    # remove old style retweet text "RT"
+    tweet = re.sub(r"^RT[\s]+", "", tweet)
+    # remove hyperlinks
+    tweet = re.sub(r"https?:\/\/.*[\r\n]*", "", tweet)
+    # remove hashtags
+    # only removing the hash # sign from the word
+    tweet = re.sub(r"#", "", tweet)
+    # tokenize tweets
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+
+    tweets_clean = []
+    for word in tweet_tokens:
+        if word not in stopwords_english and word not in string.punctuation:
+            stem_word = stemmer.stem(word)
+            tweets_clean.append(stem_word)
+
+    return tweets_clean
+
+
+def build_freqs(tweets, ys):
+    """Build frequencies.
+    Input:
+        tweets: a list of tweets
+        ys: an m x 1 array with the sentiment label of each tweet (either 0 or 1)
+    Output:
+        freqs: a dictionary mapping each (word, sentiment) pair to its frequency
+    """
+    # Convert np array to list since zip needs an iterable
+    # The squeeze is necessary or the list ends up with one element
+    # Also note that this is just a NOP if ys is alreayd a list
+    yslist = np.squeeze(ys).tolist()
+
+    # Start with an empty dictionary and populate it by looping over all tweets
+    # and over all processed words in each tweet.
+    freqs = {}
+    for y, tweet in zip(yslist, tweets):
+        for word in process_tweet(tweet):
+            pair = (word, y)
+            freqs[pair] = freqs.get(pair, 0) + 1
+
+    return freqs
+
+
+def test_lookup(func):
+    freqs = {("sad", 0): 4, ("happy", 1): 12, ("oppressed", 0): 7}
+    word = "happy"
+    label = 1
+    if func(freqs, word, label) == 12:
+        return "SUCCESS!!"
+    return "Failed Sanity Check!"
+
+
+def lookup(freqs, word, label):
+    """
+    Input:
+        freqs: a dictionary with the frequency of each pair (or tuple)
+        word: the word to look up
+        label: the label corresponding to the word
+    Output:
+        n: the number of times the worh with its corresponding label appears.
+    """
+    n = 0  # freq.get((word, label), 0)
+
+    pair = (word, label)
+    if pair in freqs:
+        n = freqs[pair]
+
+    return n
+
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor="none", **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of `x` and `y`
+    Parameters
+    ----------
+    x, y : array_like, shape (n, )
+        Input data.
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+    Returns
+    ----------
+    matplotlib.patches.Ellipse
+    Other parameters
+    ----------
+    kwargs : `~matplotlib.patches.Patch` properties
+    """
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensional dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse(
+        (0, 0),
+        width=ell_radius_x * 2,
+        height=ell_radius_y * 2,
+        facecolor=facecolor,
+        **kwargs
+    )
+
+    # Calculating the standard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the standard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = (
+        transforms.Affine2D()
+        .rotate_deg(45)
+        .scale(scale_x, scale_y)
+        .translate(mean_x, mean_y)
+    )
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
